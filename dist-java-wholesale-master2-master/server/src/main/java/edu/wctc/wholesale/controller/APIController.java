6ff4c8290ffa471fdb48e4df1f83e1cd96a4f4e7
ehinners <ehinners@my.wctc.edu>
2021-10-06 20:21:33 -0500

    added @CrossOrigin

--- dist-java-wholesale-master2-master/server/src/main/java/edu/wctc/wholesale/controller/APIController.java
+++ dist-java-wholesale-master2-master/server/src/main/java/edu/wctc/wholesale/controller/APIController.java
@@ -5,6 +5,7 @@ import edu.wctc.wholesale.entity.WholesaleOrder;
 import edu.wctc.wholesale.service.WholesaleService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/")
 public class APIController
